9c443bb732b5a5372d3ada47d01e6996f345ecd0
rb <842549829@qq.com>
2025-06-17 15:29:34 +0800

    feat:新增日志文件记录

--- src/main/java/com/his/heal/controller/TestController.java
+++ src/main/java/com/his/heal/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.his.heal.controller;
 
 import com.his.heal.services.UserServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/test")
 public class TestController {
+
+    private static final Logger logger =  LoggerFactory.getLogger(TestController.class);
 
     private final UserServices userServices;
 
@@ -18,6 +22,8 @@ public class TestController {
     @GetMapping
     public  String index(){
         try {
+            logger.error("错误测试");
+
             var user = userServices.findById("3a194f8d-d797-004e-d3b2-89c6a9684089");
             return user.getUserName();
         } catch (Exception e) {
