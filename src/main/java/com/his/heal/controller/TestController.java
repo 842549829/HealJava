eb4a21bfd8a54e090e70b34a1617b4b391c78e79
rb <842549829@qq.com>
2025-07-04 08:49:30 +0800

    feat:统一响应格式

--- src/main/java/com/his/heal/controller/TestController.java
+++ src/main/java/com/his/heal/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.his.heal.controller;
 
 import com.his.heal.services.UserServices;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/test")
+@Slf4j // 日志注解 这样写就库直接使用 log.*
 public class TestController {
 
     private static final Logger logger =  LoggerFactory.getLogger(TestController.class);
@@ -22,6 +24,8 @@ public class TestController {
     @GetMapping
     public  String index(){
         try {
+            log.info("<UNK>");
+
             logger.error("错误测试");
 
             var user = userServices.findById("3a194f8d-d797-004e-d3b2-89c6a9684089");
